e0eb10a441406d6645ffe97c23df90929f738146
Mfahrialwahyudi99 <fahrialaliastkj2@gmail.com>
2025-02-09 02:58:54 +0700

    membuat migrasi order dan migrasi cars

---
+++ app/Database/Migrations/2025-02-08-194350_AddOrdersTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'users_id' => [
+                'type'       => 'INT',
+                'constraint' => 5,
+                'unsigned'   => true,
+            ],
+           
+           'total_price'=>[
+                'type'       => 'INT',
+                'constraint' => 5,
+           ],
+           'payment_method' => [
+                'type'       => 'ENUM',
+                'constraint' => ['credit', 'cash'],
+                'default'    => 'credit',
+            ],
+            'created_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+            'updated_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('orders');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('orders');
+    }
+}
