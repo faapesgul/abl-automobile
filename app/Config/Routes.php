9c9aa5225000a644025bf0f5ccf4e1ec4f7e3b6c
Mfahrialwahyudi99 <fahrialaliastkj2@gmail.com>
2025-02-09 15:59:11 +0700

    fix routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,10 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-<<<<<<< HEAD
-=======
 $routes->get('/v1/customers', 'Customers::index');
 $routes->get('/v1/automobiles', 'Cars::index');
 $routes->get('/v1/orders', 'Orders::index');
 $routes->get('/v1/orders/(:num)', 'Orders::getOrderById/$1');
->>>>>>> dev
